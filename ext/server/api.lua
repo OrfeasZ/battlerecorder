d9272258bd61aa9c85a987e7225191e7bf302b7f
Orfeas Zafeiris <orfeaz@gmail.com>
2020-12-19 12:15:39 +0200

    Rework the playback API
    You can now load a recording in advance and then play / stop it on
    demand at a later time.

--- ext/server/api.lua
+++ ext/server/api.lua
@@ -1,4 +1,23 @@
-Events:Subscribe('br:play', function(data)
-	local replayer = Replayer(deserializeRecordingDataFromBase64(data))
-	replayer:play()
+local g_Replayers = {}
+
+Events:Subscribe('br:load', function(id, data)
+	g_Replayers[id] = Replayer(deserializeRecordingDataFromBase64(data))
+end)
+
+Events:Subscribe('br:play', function(id)
+	if g_Replayers[id] == nil then
+		error('Replay with id "' .. id .. '" is not loaded.')
+		return
+	end
+
+	g_Replayers[id]:play()
+end)
+
+Events:Subscribe('br:stop', function(id)
+	if g_Replayers[id] == nil then
+		error('Replay with id "' .. id .. '" is not loaded.')
+		return
+	end
+
+	g_Replayers[id]:stop()
 end)
